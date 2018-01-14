286e23a9bde222c6486acfb0113a7449ee0a2639
WendyKKelly <wendykkelly@gmail.com>
2018-01-13 17:29:29 -0800

    changes made to app

---
+++ app/weather-widget/constants/constants.js
@@ -0,0 +1,52 @@
+"use strict";
+exports.FORECAST_KEY = "1261fae06310293155c5caada2d2546c";
+exports.FORECAST_ROOT = "https://api.darksky.net/forecast/";
+exports.GOOGLE_KEY = "AIzaSyDsC9oiiR6Ks7g0eP313VTvrPUZ7uqJDYw ";
+exports.GOOGLE_ROOT = "https://maps.googleapis.com/maps/api/geocode/json";
+exports.WEATHER_COLORS = {
+    'default': {
+        'background-color': '#00BCD4',
+        'color': '#FFF'
+    },
+    'clear-day': {
+        'background-color': '#22A7f0',
+        'color': '#FFF'
+    },
+    'clear-night': {
+        'background-color': '#2c3e60',
+        'color': '#FFF'
+    },
+    'rain': {
+        'background-color': '#5c97bf',
+        'color': '#000'
+    },
+    'snow': {
+        'background-color': '#fff',
+        'color': '#000'
+    },
+    'sleet': {
+        'background-color': '#e4fife',
+        'color': '#000'
+    },
+    'fog': {
+        'background-color': '#67809f',
+        'color': '#FFF'
+    },
+    'wind': {
+        'background-color': '#81cfe0',
+        'color': '#FFF'
+    },
+    'cloudy': {
+        'background-color': '#bdc3c7',
+        'color': '#000'
+    },
+    'partly-cloudy-day': {
+        'background-color': '#d2d7d3',
+        'color': '#000'
+    },
+    'partly-cloudy-night': {
+        'background-color': '#34495e',
+        'color': '#000'
+    },
+};
+//# sourceMappingURL=constants.js.map
---
+++ app/weather-widget/constants/constants.ts
@@ -0,0 +1,50 @@
+export const FORECAST_KEY = "1261fae06310293155c5caada2d2546c";
+export const FORECAST_ROOT = "https://api.darksky.net/forecast/";
+export const GOOGLE_KEY = "AIzaSyDsC9oiiR6Ks7g0eP313VTvrPUZ7uqJDYw ";
+export const GOOGLE_ROOT = "https://maps.googleapis.com/maps/api/geocode/json";
+export const WEATHER_COLORS = {
+  'default': {
+  'background-color': '#00BCD4',
+  'color': '#FFF'
+},
+  'clear-day': {
+  'background-color': '#22A7f0',
+  'color': '#FFF'
+},
+  'clear-night': {
+  'background-color': '#2c3e60',
+  'color': '#FFF'
+},
+  'rain': {
+  'background-color': '#5c97bf',
+  'color': '#000'
+},
+  'snow': {
+  'background-color': '#fff',
+  'color': '#000'
+},
+  'sleet': {
+  'background-color': '#e4fife',
+  'color': '#000'
+},
+'fog': {
+'background-color': '#67809f',
+'color': '#FFF'
+},
+  'wind': {
+  'background-color': '#81cfe0',
+  'color': '#FFF'
+},
+  'cloudy': {
+  'background-color': '#bdc3c7',
+  'color': '#000'
+},
+  'partly-cloudy-day': {
+  'background-color': '#d2d7d3',
+  'color': '#000'
+},
+  'partly-cloudy-night': {
+  'background-color': '#34495e',
+  'color': '#000'
+},
+}
